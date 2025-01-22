207a123bf768b7c03347cf9b7843c7772b2c8489
Darksorrow23 <andresfelipesrango@gmail.com>
2025-01-21 19:06:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,34 @@
+require('dotenv').config();  
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const multer = require('multer');
 const Recipe = require('./models/Recipe'); 
 
-
-
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;  
 
 
-
-app.use(express.static(path.join(__dirname, 'public')));""
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const upload = multer({ dest: 'uploads/' });
 
-// Conexión a la base 
-mongoose
-  .connect('mongodb://127.0.0.1:27017/recetas', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Conectado a MongoDB'))
-  .catch((err) => console.error('Error al conectar a MongoDB:', err));
 
-// Verifica los datos que llegan
-app.post('/submit-recipe', upload.single('recta'), async (req, res) => {
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Conectado a MongoDB Atlas'))
+.catch((err) => console.error('Error al conectar a MongoDB:', err));
+
+
+app.post('/submit-recipe', upload.single('recipeFile'), async (req, res) => {
   try {
-    console.log(req.body);  
-    console.log(req.file);  
-
     const { name, email, instructions } = req.body;
-    const filePath = req.file ? req.file.path : null; //archivo
+    const filePath = req.file ? req.file.path : null;
 
     const newRecipe = new Recipe({ name, email, instructions, filePath });
     await newRecipe.save();
@@ -48,8 +40,9 @@ app.post('/submit-recipe', upload.single('recta'), async (req, res) => {
   }
 });
 
-// Iniciar el servi
+
 app.listen(PORT, () => {
-  console.log(`Servidor principal corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
+   
